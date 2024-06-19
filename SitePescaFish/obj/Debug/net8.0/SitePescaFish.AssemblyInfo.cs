8fe6876bf4217c5daffe68ac32514466bc484d7b
Nascimento <gustaveiro27@gmail.com>
2024-06-19 18:00:57 -0300

    Site terminado

--- SitePescaFish/obj/Debug/net8.0/SitePescaFish.AssemblyInfo.cs
+++ SitePescaFish/obj/Debug/net8.0/SitePescaFish.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SitePescaFish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62787e79dc79a767118d082d222247def3ae4056")]
 [assembly: System.Reflection.AssemblyProductAttribute("SitePescaFish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SitePescaFish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
